2beb122b50c2fb4f8fd53262c27409bd35c2c3c9
gerken <gerken@97d050b9-b799-4550-a901-4f04598d4601>
2009-08-14 15:57:45 +0000

    This is needed for working tests

--- osha/status/tests/base.py
+++ osha/status/tests/base.py
@@ -1,3 +1,7 @@
+from Products.Five import zcml
+from Products.Five import fiveconfigure
+from Testing import ZopeTestCase as ztc
+
 from Products.PloneTestCase import PloneTestCase as ptc
 from Products.PloneTestCase import layer
 
@@ -6,6 +10,10 @@ SiteLayer = layer.PloneSite
 class OshaStatusLayer(SiteLayer):
     @classmethod
     def setUp(cls):
+        import osha.status
+        zcml.load_config('configure.zcml', osha.status)
+        fiveconfigure.debug_mode = False
+        ztc.installPackage('osha.status', quiet=True)
         ptc.setupPloneSite(products=[])
         SiteLayer.setUp()
 
